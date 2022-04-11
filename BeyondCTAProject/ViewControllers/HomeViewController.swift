3f2dd154ae8d6ab5d473a1c58813c0a65eadbe18
TaiseiSakamoto <falcon_hmktc_9@icloud.com>
2022-04-11 23:31:29 +0900

    Add HomeViewController

---
+++ BeyondCTAProject/ViewControllers/HomeViewController.swift
@@ -0,0 +1,15 @@
+//
+//  HomeViewController.swift
+//  BeyondCTAProject
+//
+//  Created by Taisei Sakamoto on 2022/04/11.
+//
+
+import UIKit
+
+final class HomeViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
