5742471db1c715289f0861eeb1d53894ed333df0
mayachiu <mayachiu5g@gmail.com>
2022-04-25 00:48:17 +0900

    Update HomeViewController.swift

--- BeyondCTAProject/ViewControllers/HomeViewController.swift
+++ BeyondCTAProject/ViewControllers/HomeViewController.swift
@@ -8,8 +8,19 @@
 import UIKit
 
 final class HomeViewController: UIViewController {
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var searchButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        searchBar.searchTextField.backgroundColor = .white
+        searchBar.searchTextField.layer.cornerRadius = 20.0
+        searchBar.searchTextField.layer.masksToBounds = true
+        searchBar.layer.shadowColor = UIColor.gray.cgColor
+        searchBar.layer.shadowOpacity = 1
+        searchBar.layer.shadowRadius = 4
+        searchBar.layer.shadowOffset = CGSize(width: 0, height: 2)
+        
+        searchButton.layer.cornerRadius = 20.0
     }
 }
