33ac5e077d75193ae58fa59d3dec50b4190995b3
内山和輝 <mayachiu5g@gmail.com>
2022-05-21 02:46:01 +0900

    Update SearchViewController

--- BeyondCTAProject/ViewControllers/SearchViewController.swift
+++ BeyondCTAProject/ViewControllers/SearchViewController.swift
@@ -13,6 +13,10 @@ final class SearchViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureUI()
+    }
+    
+    private func configureUI() {
         searchBar.searchTextField.backgroundColor = .white
         searchBar.searchTextField.layer.cornerRadius = 20.0
         searchBar.searchTextField.layer.masksToBounds = true
@@ -20,7 +24,6 @@ final class SearchViewController: UIViewController {
         searchBar.layer.shadowOpacity = 1
         searchBar.layer.shadowRadius = 4
         searchBar.layer.shadowOffset = CGSize(width: 0, height: 2)
-        
         searchButton.layer.cornerRadius = 12.0
     }
 }
